a6971aaf11ad39cf1f7fa1129381fc5ed112b2f1
razahaider03 <54476563+razahaider03@users.noreply.github.com>
2022-08-23 19:03:52 +0530

    completed with bugs
    Reading completed with bugs

--- CharactersInPlay.java
+++ CharactersInPlay.java
@@ -9,14 +9,44 @@ import java.util.*;
 import edu.duke.*;
 
 public class CharactersInPlay {
-    private ArrayList<String> charName;
-    private ArrayList<Integer> charCount;
+    private ArrayList<String> charName = new ArrayList();
+    private ArrayList<Integer> charCount = new ArrayList();
     
     public void update(String person){
-        StorageResource sr = new StorageResource();
-        for(String s : sr.lines()){
-            
+        int currIndex = charName.indexOf(person);
+        if(currIndex == -1 ){
+            charName.add(person);
+            charCount.add(1);
+        } else {
+            int freq = charCount.get(currIndex);
+            charCount.set(currIndex,freq+1);
         }
+    }
+    
+    public void findAllCharacters(){
+        FileResource fr = new FileResource();
+        for(String person : fr.lines()){
+            int index = person.indexOf(".");
+            if (index != -1){
+                update(person);
+            }
+        }
+        int maxFreq = 0;
+        int currIndex = 0;
+        for(int k = 0; k<charCount.size();k++)
+        if (charCount.get(k)>charCount.get(k)){
+            maxFreq = charCount.get(k);
+            currIndex  = k;
+        }
+        System.out.println("Most Frequent Name " 
+                            + charName.get(currIndex) +"\t" 
+                            + charCount.get(currIndex));
+        charName.clear();
+        charCount.clear();
+    }
+    
+    public void tester(){
+        findAllCharacters();
     }
 
 }
